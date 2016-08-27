b18245b81c92ed9efaf54e0d71a834dff29b8386
Daniel Whiffing <danielwhiffing@gmail.com>
2016-08-27 09:34:57 -0400

    Pulls Unit out of Spawner

--- src/entities/spawner.js
+++ src/entities/spawner.js
@@ -1,36 +1,19 @@
+import Unit from './Unit'
+
 export default class Spawner {
   constructor(game) {
     this.game = game
-    this.units = game.add.group()
-
-    for (let x = 0; x < 40; x++) {
-      let unit = this.units.create(0, 0, 'tiles')
-      unit.width = 15
-      unit.height = 30
-      unit.kill()
-      this.randomizeColor(unit)
-    }
+    this.units = game.add.group(undefined, 'units', false, true)
+    this.units.classType = Unit
+    this.units.createMultiple(40, 'tiles')
   }
 
   update() {
-    this.units.children.forEach((unit) => {
-      if (unit.alive) {
-        unit.x += unit.speed
-        if (unit.x > 800) {
-          unit.kill()
-        }
-      }
-    })
+    this.units.callAll('update')
   }
 
   spawn(frame) {
     let unit = this.units.getFirstDead()
-    unit.reset(-10, 360)
-    unit.speed = this.game.rnd.integerInRange(100, 400) / 100
-    unit.frame = frame
-  }
-
-  randomizeColor(tile) {
-    tile.frame = this.game.rnd.integerInRange(0, tile.animations.frameTotal - 1)
+    unit.reset(-10, 360, frame)
   }
 }
--- src/entities/unit.js
+++ src/entities/unit.js
@@ -1,9 +1,32 @@
-export default class Unit {
-  constructor(game) {
+import Phaser from 'phaser'
+
+export default class Unit extends Phaser.Sprite {
+  constructor(game, x, y, key, frame) {
+    super(game, x, y, key, frame)
     this.game = game
+    this.width = 15
+    this.height = 30
+    this.kill()
+  }
+
+  randomType() {
+    return this.game.rnd.integerInRange(0, this.animations.frameTotal - 1)
+  }
+
+  reset(x, y, frame) {
+    this.frame = frame || this.randomType()
+    this.speed = this.game.rnd.integerInRange(100, 400) / 100
+    super.reset(x, y)
   }
 
   update() {
+    if (!this.alive) {
+      return
+    }
 
+    if (this.x > 800) {
+      this.kill()
+    }
+    this.x += this.speed
   }
 }
