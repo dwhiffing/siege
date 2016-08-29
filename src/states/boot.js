72aa0c9439c9ed55b69fbecf522ec1ac39c05872
Daniel Whiffing <danielwhiffing@gmail.com>
2016-08-28 22:13:41 -0400

    ready for prod

--- src/states/boot.js
+++ src/states/boot.js
@@ -1,5 +1,12 @@
 export default {
   create(game) {
+    //  This sets a limit on the up-scale
+    this.game.scale.maxWidth = 900;
+    this.game.scale.maxHeight = 560;
+
+    //  Then we tell Phaser that we want it to scale up to whatever the browser can handle, but to do it proportionally
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    // this.game.scale.setScreenSize();
     this.game.stage.smoothed = false
     this.game.state.start('load', true, false)
   }
