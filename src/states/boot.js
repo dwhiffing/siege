47e1e685acf4476f2735356c80259f62de732f78
Daniel Whiffing <danielwhiffing@gmail.com>
2020-04-26 22:51:43 -0400

    Adds loader

--- src/states/boot.js
+++ src/states/boot.js
@@ -1,13 +1,15 @@
 export default {
   create(game) {
     //  This sets a limit on the up-scale
-    this.game.scale.maxWidth = 900;
-    this.game.scale.maxHeight = 560;
+    this.game.scale.maxWidth = 900
+    this.game.scale.maxHeight = 560
 
     //  Then we tell Phaser that we want it to scale up to whatever the browser can handle, but to do it proportionally
-    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
     // this.game.scale.setScreenSize();
     this.game.stage.smoothed = false
+    this.load.image('loader', 'images/loader.png')
+
     this.game.state.start('load', true, false)
-  }
+  },
 }
--- src/states/load.js
+++ src/states/load.js
@@ -1,6 +1,8 @@
 export default {
   preload() {
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this)
+    let loader = this.add.sprite(150, 150, 'loader')
+    this.load.setPreloadSprite(loader)
 
     this.load.spritesheet('tiles', 'assets/images/tiles.png', 75, 75)
     this.load.spritesheet('units', 'assets/images/units.png', 15, 30)
@@ -18,22 +20,22 @@ export default {
     this.load.image('ground', 'assets/images/ground.png')
     this.load.image('sky', 'assets/images/sky.jpg')
 
-    this.load.audio('medium_crash_1','assets/audio/medium_crash_1.mp3')
-    this.load.audio('medium_crash_2','assets/audio/medium_crash_2.mp3')
-    this.load.audio('medium_crash_7','assets/audio/medium_crash_7.mp3')
-    this.load.audio('small_crash_1','assets/audio/small_crash_1.mp3')
-    this.load.audio('small_crash_2','assets/audio/small_crash_2.mp3')
-    this.load.audio('small_crash_3','assets/audio/small_crash_3.mp3')
-    this.load.audio('footstep','assets/audio/footstep.mp3')
-    this.load.audio('pick','assets/audio/pick.mp3')
-    this.load.audio('submit','assets/audio/submit.mp3')
-    this.load.audio('swap','assets/audio/swap.mp3')
-    this.load.audio('swap2','assets/audio/swap2.mp3')
-    this.load.audio('swipe','assets/audio/swipe.mp3')
-    this.load.audio('submit_failed','assets/audio/submit_failed.mp3')
+    this.load.audio('medium_crash_1', 'assets/audio/medium_crash_1.mp3')
+    this.load.audio('medium_crash_2', 'assets/audio/medium_crash_2.mp3')
+    this.load.audio('medium_crash_7', 'assets/audio/medium_crash_7.mp3')
+    this.load.audio('small_crash_1', 'assets/audio/small_crash_1.mp3')
+    this.load.audio('small_crash_2', 'assets/audio/small_crash_2.mp3')
+    this.load.audio('small_crash_3', 'assets/audio/small_crash_3.mp3')
+    this.load.audio('footstep', 'assets/audio/footstep.mp3')
+    this.load.audio('pick', 'assets/audio/pick.mp3')
+    this.load.audio('submit', 'assets/audio/submit.mp3')
+    this.load.audio('swap', 'assets/audio/swap.mp3')
+    this.load.audio('swap2', 'assets/audio/swap2.mp3')
+    this.load.audio('swipe', 'assets/audio/swipe.mp3')
+    this.load.audio('submit_failed', 'assets/audio/submit_failed.mp3')
   },
 
   onLoadComplete() {
     this.game.state.start('menu')
-  }
+  },
 }
